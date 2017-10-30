20caff11037aa1219178a44a661e3523884f0645
trentonsweat <trentonsweat@gmail.com>
2017-10-30 15:59:27 -0400

    first commit for test

---
+++ FinalAssessmentNumber1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FinalAssessmentNumber1.MvcApplication" Language="C#" %>
---
+++ FinalAssessmentNumber1/Models/UserData.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalAssessmentNumber1.Models
+{
+    public class UserData
+    {
+        private int num1;
+        private int num2;
+      
+
+
+        public int Num1
+        {
+            set { num1 = value; }
+            get { return num1; }
+        }
+
+        public int Num2
+        {
+            set { num2 = value; }
+            get { return num2; }
+        }
+
+        
+
+        //default constructor
+        public UserData()
+        {
+            Num1 = 0;
+
+            Num2 = 0;
+
+            
+        }
+
+        //overloaded constructor
+        public UserData(int n1, int n2)
+        {
+            Num1 = n1;
+
+            Num2 = n2;
+
+           
+        }
+
+
+    }
+}
